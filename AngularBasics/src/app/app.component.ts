d56201724623ca338cfdc0b770f2e4c29168e2de
Dana Bosoanca <dana_bosoanca@yahoo.com>
2023-05-04 14:38:40 +0300

    Angular Basics

---
+++ AngularBasics/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'AngularBasics';
+  message =
+    'Angular is the best JS framework for building client-side application';
+}
