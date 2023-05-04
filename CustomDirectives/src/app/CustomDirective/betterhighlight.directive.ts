05fffcd5c25de546a906d4a9d4985c1beba6556f
Dana Bosoanca <dana_bosoanca@yahoo.com>
2023-05-04 15:40:15 +0300

    Binding Directive Properties

--- CustomDirectives/src/app/CustomDirective/betterhighlight.directive.ts
+++ CustomDirectives/src/app/CustomDirective/betterhighlight.directive.ts
@@ -3,25 +3,35 @@ import {
   ElementRef,
   HostBinding,
   HostListener,
+  Input,
+  OnInit,
   Renderer2,
 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterhighlight]',
 })
-export class BetterhighlightDirective {
+export class BetterhighlightDirective implements OnInit {
   constructor(private element: ElementRef, private renderer: Renderer2) {}
 
-  @HostBinding('style.backgroundColor') background: string = 'transparent';
+  @Input() defaultColor: string = 'transparent';
+  @Input('appBetterhighlight') highlightColor: string = 'pink';
+  @Input() title: string = 'This is title';
+
+  @HostBinding('style.backgroundColor') background: string = this.defaultColor;
   @HostBinding('style.border') border: string = 'none';
 
+  ngOnInit() {
+    this.background = this.defaultColor;
+  }
+
   @HostListener('mouseenter') mouseenter() {
-    this.background = 'pink';
+    this.background = this.highlightColor;
     this.border = 'red 2px solid';
   }
 
   @HostListener('mouseleave') mouseleave() {
-    this.background = 'transparent';
+    this.background = this.defaultColor;
     this.border = 'none';
   }
 }
