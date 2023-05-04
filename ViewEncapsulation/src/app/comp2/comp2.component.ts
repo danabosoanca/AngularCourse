6f336cba51bea530746a8490244c709222300242
Dana Bosoanca <dana_bosoanca@yahoo.com>
2023-05-04 15:16:14 +0300

    View Encapsulation

---
+++ ViewEncapsulation/src/app/comp2/comp2.component.ts
@@ -0,0 +1,9 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-comp2',
+  templateUrl: './comp2.component.html',
+  styleUrls: ['./comp2.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom,
+})
+export class Comp2Component {}
